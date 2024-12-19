6be63f7d0d9159b4665f8acc4c6493951d6f7f4d
Stephen Paul <Stephenpaul4040@gmail.com>
2024-12-19 11:20:53 +0530

    Updated Database Refernces

--- Database_Reference.sql
+++ Database_Reference.sql
@@ -4,18 +4,16 @@ ALTER DATABASE employee_db OWNER TO postgres;
 
 \connect employee_db
 
-
-CREATE TABLE public.employees (
-    name character varying(255) NOT NULL,
-    email character varying(255) NOT NULL,
-    phone character varying(255) NOT NULL,
-    dob date NOT NULL,
-    age integer NOT NULL,
-    gender character varying(10) NOT NULL,
-    employee_id character varying(20) NOT NULL,
-    department character varying(50) NOT NULL,
-    other_department character varying(50),
-    date_of_joining date NOT NULL,
-    role character varying(50) NOT NULL,
-    CONSTRAINT employees_age_check CHECK ((age >= 18))
+CREATE TABLE employees (
+  employee_id VARCHAR(20) PRIMARY KEY,
+  name VARCHAR(255) NOT NULL,
+  email VARCHAR(255) UNIQUE NOT NULL,
+  phone VARCHAR(255) UNIQUE NOT NULL,
+  department VARCHAR(50) NOT NULL,
+  other_department VARCHAR(50),
+  date_of_joining DATE NOT NULL,
+  role VARCHAR(50) NOT NULL,
+  dob DATE NOT NULL,
+  age INTEGER NOT NULL,
+  gender VARCHAR(10) NOT NULL
 );
